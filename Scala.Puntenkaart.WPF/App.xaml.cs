c093c63fc1c24924ee5295f69769865a61205df4
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-12-08 13:38:55 +0100

    Minor issues resolved

--- Scala.Puntenkaart.WPF/App.xaml.cs
+++ Scala.Puntenkaart.WPF/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Prb.Puntenkaart.WPF
+namespace Scala.Puntenkaart.WPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
